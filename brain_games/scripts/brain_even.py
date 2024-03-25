175570883ae008233fa7019eed5f88b7a91e82f6
Vladimir Yashnikov <yashnikov@yashnikov.com>
2024-03-25 17:13:35 +0300

    refactor brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,49 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
-import random
-
-
-def is_even(number):
-    return number % 2 == 0
-
-
-def play_even(name):
-
-    round = 0
-    score = 0
-
-    while round < 3:
-        random_integer = random.randint(1, 20)
-        print('Answer "yes" if the number is even, otherwise answer "no".')
-        print('Question:', random_integer)
-
-        # correct answer
-        correct_answer = ''
-        if is_even(random_integer):
-            correct_answer = "yes"
-        else:
-            correct_answer = "no"
-
-        user_choice = input()
-
-        if user_choice == correct_answer:
-            score += 1
-            print('Correct!')
-        else:
-            print(
-                f"'{user_choice}' is wrong answer ;(. "
-                f"Correct answer was '{correct_answer}'"
-            )
-            break
-
-        if score == 3:
-            print('Congratulations,', name)
-            break
+from brain_games.games.brain_even import play_even
 
 
 def main():
-    name = welcome_user()
-    play_even(name)
+    play_even()
 
 
 if __name__ == '__main__':
