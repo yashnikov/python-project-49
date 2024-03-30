e08218904ddadc2dfe27b15ed5ce322c4f0dacdc
Vladimir Yashnikov <yashnikov@yashnikov.com>
2024-03-30 09:24:09 +0300

    remove magic numbers

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,22 +2,24 @@ import prompt
 
 
 def run_game(get_question_and_answer, description):
+
+    SCORE_LIMIT = 3
+    ROUNDS_NUMBER = 3
     score = 0
-    rounds_number = 3
 
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}")
     print(description)
 
-    for _ in range(rounds_number):
+    for _ in range(ROUNDS_NUMBER):
         question, correct_answer = get_question_and_answer()
         print('Question:', question)
         user_answer = prompt.string('Your answer: ')
         if user_answer == correct_answer:
             score += 1
             print('Correct!')
-            if score == 3:
+            if score == SCORE_LIMIT:
                 print('Congratulations,', name)
                 break
         else:
